33c69d8701114834a2778f6cbd1af969ac6e0f07
spatten <scott@fossa.com>
2022-03-07 23:24:10 -0800

    add the decreaseTodo command

--- src/extension.ts
+++ src/extension.ts
@@ -8,53 +8,85 @@ import * as path from 'path';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-todos" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "markdown-todos" is now active!');
 
-	let increaseTodo = vscode.commands.registerCommand('markdown-todos.increaseTodo', async () => {
-		let editor = vscode.window.activeTextEditor;
-		if (!editor) { return; }
+  let increaseTodo = vscode.commands.registerCommand('markdown-todos.increaseTodo', async () => {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) { return; }
 
-		let lineIndex = editor.selection.active.line;
-		let lineText = editor.document.lineAt(lineIndex).text;
+    let lineIndex = editor.selection.active.line;
+    let lineText = editor.document.lineAt(lineIndex).text;
 
-		// Return if it's not a header
-		if (!lineText.match(/^\s*#/)) { return; }
+    // Return if it's not a header
+    if (!lineText.match(/^\s*#/)) { return; }
 
-		const match = /(^\s*)([#]+)(\s*)([^ ]*)/.exec(lineText);
-		if (!match) { return; }
-		const [leading, octos, trailing, firstWord] = match.slice(1);
-		return await editor.edit((editBuilder) => {
-			const startPos = (leading + octos + trailing).length;
-			const endPos = (leading + octos + trailing + firstWord).length + 1;
-			const replaceRange = new vscode.Range(new vscode.Position(lineIndex, startPos), new vscode.Position(lineIndex, endPos));
-			switch (firstWord) {
-				case 'TODO':
-					editBuilder.replace(replaceRange, 'DONE ');
-					break;
-				case 'DONE':
-					editBuilder.replace(replaceRange, '');
-					break;
-				default:
-					editBuilder.insert(new vscode.Position(lineIndex, startPos), 'TODO ');
-			}
-		});
-	});
+    const match = /(^\s*)([#]+)(\s*)([^ ]*)/.exec(lineText);
+    if (!match) { return; }
+    const [leading, octos, trailing, firstWord] = match.slice(1);
+    return await editor.edit((editBuilder) => {
+      const startPos = (leading + octos + trailing).length;
+      const endPos = (leading + octos + trailing + firstWord).length + 1;
+      const replaceRange = new vscode.Range(new vscode.Position(lineIndex, startPos), new vscode.Position(lineIndex, endPos));
+      switch (firstWord) {
+        case 'TODO':
+          editBuilder.replace(replaceRange, 'DONE ');
+          break;
+        case 'DONE':
+          editBuilder.replace(replaceRange, '');
+          break;
+        default:
+          editBuilder.insert(new vscode.Position(lineIndex, startPos), 'TODO ');
+      }
+    });
+  });
 
-	context.subscriptions.push(increaseTodo);
+  context.subscriptions.push(increaseTodo);
 
-	let openCurrentWorklog = vscode.commands.registerCommand('markdown-todos.openCurrentWorklog', async () => {
-		const worklogDir = "/Users/scott/Dropbox/work-logs";
-		const entries = await fs.promises.readdir(worklogDir);
-		const workLogs = entries.filter(entry => entry.match(/\d\d\d\d-\d\d-\d\d\.md/));
-		const currentLog = workLogs.sort()[0];
-		let uri = vscode.Uri.file(path.join(worklogDir, currentLog));
-		await vscode.commands.executeCommand('vscode.open', uri);
-		vscode.commands.executeCommand('editor.foldAll');
-	});
+  let decreaseTodo = vscode.commands.registerCommand('markdown-todos.decreaseTodo', async () => {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) { return; }
 
-	context.subscriptions.push(openCurrentWorklog);
+    let lineIndex = editor.selection.active.line;
+    let lineText = editor.document.lineAt(lineIndex).text;
+
+    // Return if it's not a header
+    if (!lineText.match(/^\s*#/)) { return; }
+
+    const match = /(^\s*)([#]+)(\s*)([^ ]*)/.exec(lineText);
+    if (!match) { return; }
+    const [leading, octos, trailing, firstWord] = match.slice(1);
+    return await editor.edit((editBuilder) => {
+      const startPos = (leading + octos + trailing).length;
+      const endPos = (leading + octos + trailing + firstWord).length + 1;
+      const replaceRange = new vscode.Range(new vscode.Position(lineIndex, startPos), new vscode.Position(lineIndex, endPos));
+      switch (firstWord) {
+        case 'TODO':
+          editBuilder.replace(replaceRange, '');
+          break;
+        case 'DONE':
+          editBuilder.replace(replaceRange, 'TODO ');
+          break;
+        default:
+          editBuilder.insert(new vscode.Position(lineIndex, startPos), 'DONE ');
+      }
+    });
+  });
+
+  context.subscriptions.push(decreaseTodo);
+
+  let openCurrentWorklog = vscode.commands.registerCommand('markdown-todos.openCurrentWorklog', async () => {
+    const worklogDir = "/Users/scott/Dropbox/work-logs";
+    const entries = await fs.promises.readdir(worklogDir);
+    const workLogs = entries.filter(entry => entry.match(/\d\d\d\d-\d\d-\d\d\.md/));
+    const currentLog = workLogs.sort()[0];
+    let uri = vscode.Uri.file(path.join(worklogDir, currentLog));
+    await vscode.commands.executeCommand('vscode.open', uri);
+    vscode.commands.executeCommand('editor.foldAll');
+  });
+
+  context.subscriptions.push(openCurrentWorklog);
 }
 
 // this method is called when your extension is deactivated
