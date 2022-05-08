bb5df61a0f5bf65e1e2742e724c3a528553611af
spatten <scott@fossa.com>
2022-05-08 14:16:38 -0700

    more tests for gotoNextheader and gotoPreviousHeader

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -9,15 +9,19 @@ function sleep(ms: number): Promise<void> {
   });
 }
 
-export async function openExample(exampleName: string, params: { lineNumber?: number }): Promise<vscode.TextEditor> {
+export async function openExample(exampleName: string, params?: { lineNumber?: number }): Promise<vscode.TextEditor> {
   const uri = vscode.Uri.file(path.join(__dirname + fixtureLocation + exampleName));
   const document = await vscode.workspace.openTextDocument(uri);
   const editor = await vscode.window.showTextDocument(document);
   await sleep(500);
-  if (params.lineNumber !== undefined) {
-    const newPosition = new vscode.Position(params.lineNumber, 0);
-    const newSelection = new vscode.Selection(newPosition, newPosition);
-    editor.selection = newSelection;
+  if (params?.lineNumber !== undefined) {
+    gotoLine(editor, params.lineNumber);
   }
   return editor;
+}
+
+export function gotoLine(editor: vscode.TextEditor, line: number) {
+  const newPosition = new vscode.Position(line, 0);
+  const newSelection = new vscode.Selection(newPosition, newPosition);
+  editor.selection = newSelection;
 }
--- src/test/suite/gotoHeader.test.ts
+++ src/test/suite/gotoHeader.test.ts
@@ -2,21 +2,52 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as helpers from '../helpers';
 
+suite('gotoPreviousHeader', async () => {
+  let editor: vscode.TextEditor;
+  setup(async function () {
+    editor = await helpers.openExample('simple.md', { lineNumber: 23 });
+  });
 
-suite('gotoHeader', async () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  test('gotoPreviousHeader goes to the previous header', async () => {
+    helpers.gotoLine(editor, 23);
+    await vscode.commands.executeCommand("markdown-worklogs.gotoPreviousHeader");
+    const position = editor.selection.active;
+    assert.strictEqual(21, position.line);
+  });
 
-	test('gotoPreviousHeader goes to the previous header', async () => {
-		const editor = await helpers.openExample('simple.md', { lineNumber: 23 });
-		await vscode.commands.executeCommand("markdown-worklogs.gotoPreviousHeader");
-		const position = editor.selection.active;
-		assert.strictEqual(21, position.line);
-	});
+  test('gotoPreviousHeader goes to the previous header even if you are on a header', async () => {
+    helpers.gotoLine(editor, 2);
+    await vscode.commands.executeCommand("markdown-worklogs.gotoPreviousHeader");
+    const position = editor.selection.active;
+    assert.strictEqual(0, position.line);
+  });
 
-	test('gotoPreviousHeader ignores "# " in a code block', async () => {
-		const editor = await helpers.openExample('simple.md', { lineNumber: 17 });
-		await vscode.commands.executeCommand("markdown-worklogs.gotoPreviousHeader");
-		const position = editor.selection.active;
-		assert.strictEqual(8, position.line);
-	});
+  test('gotoPreviousHeader ignores "# " in a code block', async () => {
+    helpers.gotoLine(editor, 17);
+    await vscode.commands.executeCommand("markdown-worklogs.gotoPreviousHeader");
+    const position = editor.selection.active;
+    assert.strictEqual(8, position.line);
+  });
+});
+
+suite('gotoNextHeader', async () => {
+  let editor: vscode.TextEditor;
+  setup(async function () {
+    editor = await helpers.openExample('simple.md');
+  });
+
+  test('gotoNextHeader goes to the next header', async () => {
+    helpers.gotoLine(editor, 22);
+    await vscode.commands.executeCommand("markdown-worklogs.gotoNextHeader");
+    const position = editor.selection.active;
+    assert.strictEqual(25, position.line);
+  });
+
+  test('gotoNextHeader ignores "# " in a code block', async () => {
+    // editor = await helpers.openExample('simple.md', { lineNumber: 23 });
+    helpers.gotoLine(editor, 10);
+    await vscode.commands.executeCommand("markdown-worklogs.gotoNextHeader");
+    const position = editor.selection.active;
+    assert.strictEqual(19, position.line);
+  });
 });
