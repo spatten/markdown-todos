ffc7be8d85e306811b0bfb2e95d5807e61d160a8
spatten <scott@fossa.com>
2022-05-08 23:37:11 -0700

    adding another sorting test

--- src/test/suite/sortDoneToBottom.test.ts
+++ src/test/suite/sortDoneToBottom.test.ts
@@ -5,17 +5,21 @@ import * as helpers from '../helpers';
 
 
 describe('sortDoneToBottom', async function () {
-  let editor: vscode.TextEditor;
-  let document: vscode.TextDocument;
-  before(async function () {
-    editor = await helpers.openExample('sortable.md');
-  });
-
-  it('should sort the DONEs to the bottom', async function () {
+  it('should sort the DONEs in sortable.md to the bottom', async function () {
+    const editor = await helpers.openExample('sortable.md');
     await vscode.commands.executeCommand('markdown-worklogs.sortDoneToBottom');
     await helpers.sleep(100);
     const fullText = editor.document.getText();
     const expectedSortedTextBuffer = await fs.promises.readFile(helpers.fixturePath('sortable-sorted.md'));
+    const expectedSortedText = expectedSortedTextBuffer.toString();
+    assert.strictEqual(fullText, expectedSortedText);
+  });
+  it('should sort the DONEs in another-sortable.md to the bottom', async function () {
+    const editor = await helpers.openExample('another-sortable.md');
+    await vscode.commands.executeCommand('markdown-worklogs.sortDoneToBottom');
+    await helpers.sleep(100);
+    const fullText = editor.document.getText();
+    const expectedSortedTextBuffer = await fs.promises.readFile(helpers.fixturePath('another-sortable-sorted.md'));
     const expectedSortedText = expectedSortedTextBuffer.toString();
     assert.strictEqual(fullText, expectedSortedText);
   });
