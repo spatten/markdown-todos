b880fb57618f2fc76e060ca88a0e6ead1524f98a
Scott Patten <scott@fossa.com>
2022-05-11 01:28:50 -0700

    Eslint in CI (#9)

--- src/extension.ts
+++ src/extension.ts
@@ -358,11 +358,6 @@ const getConfig = (param: string): string | undefined => vscode.workspace.getCon
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "markdown-worklogs" is now active!');
-
-
   let increaseTodo = vscode.commands.registerTextEditorCommand('markdown-worklogs.increaseTodo', async (te, edit) => {
     await changeTodo(te, edit, 1);
   });
